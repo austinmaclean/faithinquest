4d5a336822021778035709fc236fcf09db4879dc
gleb44 <gleb44a@gmail.com>
2016-06-15 15:18:29 +0300

    Ability to add title, description, speaker, video link, start time

--- db/patches/patch_001.sql
+++ db/patches/patch_001.sql
@@ -1,3 +1,16 @@
 -- First
+create table study (
+    id int8 not null,
+    created timestamp not null,
+    description varchar(255),
+    link varchar(255) not null,
+    speaker varchar(255),
+    start_min int4 not null,
+    start_sec int4 not null,
+    title varchar(255) not null,
+    primary key (id)
+);
+
+create sequence study_seq start 1 increment 1;
 
 insert into patch_log (version) values (1);
