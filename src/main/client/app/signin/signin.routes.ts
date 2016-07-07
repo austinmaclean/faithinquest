92f3473d44b5770d5d9758a4b1b601747447dbfe
gyazykov <gyazykov@gmail.com>
2016-07-07 15:57:34 +0300

    Fix admin UI load

--- src/main/client/app/signin/signin.routes.ts
+++ src/main/client/app/signin/signin.routes.ts
@@ -1,9 +1,11 @@
-import { AuthGuard }          from '../shared/auth/auth.guard';
-import { AccountService }        from '../shared/service/account.service';
-import { SignInComponent }     from './signin.component';
+import {AuthGuard}          from '../shared/auth/auth.guard';
+import {AccountService}        from '../shared/service/account.service';
+import {HttpLoaderService}        from '../shared/http-loader/httpLoaderService';
+import {Notification}        from '../shared/notification/notification';
+import {SignInComponent}     from './signin.component';
 
 export const SignInRoutes = [
-    { path: 'login', component: SignInComponent }
+    {path: 'login', component: SignInComponent}
 ];
 
-export const AUTH_PROVIDERS = [AuthGuard, AccountService];
+export const AUTH_PROVIDERS = [AuthGuard, AccountService, HttpLoaderService, Notification];
